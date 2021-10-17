27ca729a838e079b50c59bbc0fff1680ef8bdcca
Tamás Túri <ttturi@gmail.com>
2021-10-18 01:42:27 +0200

    How the C++ Linker Works - Wrongly named definiton in Log.cpp, still works because Log is declared, but never called.

--- HelloWorld/HelloWorld/Log.cpp
+++ HelloWorld/HelloWorld/Log.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
-void Log(const char* message)
+void Logr(const char* message)
 {
 	std::cout << message << std::endl;
 }
--- HelloWorld/HelloWorld/Math.cpp
+++ HelloWorld/HelloWorld/Math.cpp
@@ -4,7 +4,7 @@ void Log(const char* message);
 
 int Multiply(int a, int b)
 {
-	Log("Multiply");
+	//Log("Multiply");
 	return a * b;
 }
 
