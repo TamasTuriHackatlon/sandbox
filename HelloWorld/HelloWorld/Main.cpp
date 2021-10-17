567505dc17756d2d590e7a3a8e5275e5f8dfdd35
Tamás Túri <ttturi@gmail.com>
2021-10-17 19:41:04 +0200

    How C++ Works - Log function body/definition is in a separate Log.cpp file

--- HelloWorld/HelloWorld/Main.cpp
+++ HelloWorld/HelloWorld/Main.cpp
@@ -1,9 +1,7 @@
 #include <iostream>
 
-void Log(const char* message)
-{
-	std::cout << message << std::endl;
-}
+//Declaration of the Log function is needed. The body, or the definition of the Log function is located in Log.cpp. Linker can make find it, as it is part of the project!
+void Log(const char* message);
 
 int main()
 {
