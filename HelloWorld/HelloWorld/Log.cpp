f11ae2ffc903c587d3144be3579fc8daf3734b84
Tamás Túri <ttturi@gmail.com>
2021-10-25 00:54:15 +0200

    C++ Header Files - Using the #ifndef #endif header guard to avoid type redefinition (#include twice the same header file for some reason)

--- HelloWorld/HelloWorld/Log.cpp
+++ HelloWorld/HelloWorld/Log.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "Log.h"
+#include "Common.h"
 
 void InitLog()
 {
--- HelloWorld/HelloWorld/Log.h
+++ HelloWorld/HelloWorld/Log.h
@@ -1,5 +1,14 @@
-#pragma once
+//#pragma once //Header guard
+//
+//void Log(const char* message);
+//void InitLog();
+//class Player {}; //Class or Struct objects shall not be redefined twice, this will cause an error
+
+#ifndef _LOG_H//Header guard in an old way, before pragma once was introduced. It checks if _LOG_H was defined before (with the #define command). So it prevents double definition.
+#define _LOG_H
 
 void Log(const char* message);
-
 void InitLog();
+class Player {};
+
+#endif
