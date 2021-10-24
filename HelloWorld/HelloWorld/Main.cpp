59bb39fae385c99bff263496bbfc5bb12f775287
Tamás Túri <ttturi@gmail.com>
2021-10-25 00:31:00 +0200

    C++ Header Files - Using header file and move all functions into a separate cpp file from the entry point.

--- HelloWorld/HelloWorld/Main.cpp
+++ HelloWorld/HelloWorld/Main.cpp
@@ -1,13 +1,9 @@
 #include <iostream>
-
-
-void Log(const char* message)
-{
-	std::cout << message << std::endl;
-}
+#include "Log.h"
 
 int main()
 {
+	InitLog();
 	Log("Hello World");
 	system("pause");
 }
